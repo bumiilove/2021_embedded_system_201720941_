ef5e2c3784859fdbd57ea94c7400b12a1dea99da
GaaangBeom <bumiilove@gmail.com>
2021-12-04 01:21:31 +0900

    아두이노 서버 함수 수정

--- 최종설계프로젝트/Project_server/Project_server.ino
+++ 최종설계프로젝트/Project_server/Project_server.ino
@@ -11,10 +11,16 @@
 #define CHARACTERISTIC_UUID "0d563a58-196a-48ce-ace2-dfec78acc814" 
 //
 
+/* 전역변수 설정 */
 int norepeat_R=0;
 int norepeat_P=0;
-String helmet ; //signal from serial in
-
+// 경범 추가
+String rx_data ;// 헬멧 착용 감지상태를 입력받는 변수(시리얼 수신)1: 감지,0:미감지,-1:횟수 오버->4로 전달
+int tx_data;    // 시리얼 송신으로 정보를 전달하는 변수 (시리얼 송신)1: 헬멧 감지 후 사용 시작알림, 8: 인식 횟수 초과로 사용불가 알림
+int state = 0;  // 킥보드의 동작 상태를 표현하는 변수 0: /on 1:running/ stop: 2/ off: 4 
+int cost = 0;   // 요금 변수 
+int finish =0;  // 킥보드의 사용이 종료됐음을 표현하는 변수 0: 동작중 1: 사용 종료
+int park =0 ;   // 주차 플래그 0: 주차spot에 주차하지 않음, 1: 주차spot에 주차함 
 // LED Matrix 드라이버 제어를 위한 라이브러리
 #include <Adafruit_GFX.h> 
 #include <Adafruit_IS31FL3731.h>
@@ -133,18 +139,55 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // 헬멧 감지 여부를 시리얼 통신으로 받음: 감지=1 미감지=0 횟수 초과 =-1
-  helmet = Serial.readStringUntil('\n');
-  if (helmet == "1"){
-                Serial.print(helmet);
-                matrix.setRotation(0);
+    // 헬멧 감지 여부를 시리얼 수신: 감지=1 미감지=0 횟수 초과 =-1
+    rx_data = Serial.readStringUntil('\n');
+  
+    // 헬멧 미착용시: 매트릭스에 S를 띄우고 상태는 유지.
+    if((state == 0 || state == 2) && rx_data =="0"){
+                state = 2;
+                //S 만 출력
+    }
+    // 헬멧 착용시 : 매트릭스에 R을 띄우고 시리얼 송신: 1 (킥보드 사용시작 : state =>1)
+    else if ((state == 0 || state == 2) && rx_data == "1"){
+                state = 1;
+                matrix.setRotation(0); 
                 matrix.clear();
                 matrix.drawBitmap(0,0,bmp_R,8,8,128); 
+                
+                tx_data = 1;
+                Serial.println(tx_data);
+                delay(100);
     }
-    else if(helmet == "-1"){
-                Serial.print(helmet);
+    // 횟수 초과시: state를 4로 바꾸는 동시에 finish도 1로 바꿈, tx_data는 4로 바꿔 사용할 수 없음을 사운드로 알림 
+    else if(state == 2 && rx_data == "-1"){
+                tx_data = 4;
+                state = 4;
+                finish = 1;
+                Serial.println(tx_data);
     }
-  
+    // 종료시    : state를 4로 바꿔줌
+    else if(state == 1&& rx_data == "4"){
+      state = 4;
+    }
+    // 사용자가 직접 킥보드를 종료한 경우 (finish =0 -> 1)
+    else if(state == 4 && finish ==0){
+      // 주차를 한 경우 cost를 500원 절감
+      if(park == 1){
+        cost -= 500;
+        }
+      Serial.println(cost);
+      finish = 1;
+      state = 0;
+      matrix.setRotation(0);
+      matrix.clear();
+      matrix.drawBitmap(0,0,bmp_P,8,8,128);
+    }
+
+    // 킥보드의 상태가 running일 경우 요금은 계속 100원씩 증가 
+    if(state == 1){
+      cost += 100;
+    }
+
+
   delay(2000);
 }
