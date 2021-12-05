ad2e1b8a0662dc62bb6f46dda2c32e94027a614f
GaaangBeom <bumiilove@gmail.com>
2021-12-05 17:20:40 +0900

    server_final

--- 최종설계프로젝트/Project_server/Project_server.ino
+++ 최종설계프로젝트/Project_server/Project_server.ino
@@ -12,14 +12,14 @@
 //
 
 /* 전역변수 설정 */
-int norepeat_R=0;
-int norepeat_P=0;
+int norepeat_B=0;
 // 경범 추가
-String rx_data ;// 헬멧 착용 감지상태를 입력받는 변수(시리얼 수신)1: 감지,0:미감지,-1:횟수 오버->4로 전달
+String rx_data ;// 헬멧 착용 감지상태를 입력받는 변수(시리얼 수신)1: 감지,0:미감지,-1:횟수 오버->4로 전달 // 날씨 데이터 7: 맑음/ 8: 맑지 않은 경우
 int tx_data;    // 시리얼 송신으로 정보를 전달하는 변수 (시리얼 송신)1: 헬멧 감지 후 사용 시작알림, 8: 인식 횟수 초과로 사용불가 알림
 int state = 0;  // 킥보드의 동작 상태를 표현하는 변수 0: /on 1:running/ stop: 2/ park: 3/ off: 4 
 int cost = 0;   // 요금 변수 
 int battery = 100;// 잔여 배터리양 
+int battery_print = 0;
 int finish =0;  // 킥보드의 사용이 종료됐음을 표현하는 변수 0: 동작중 1: 사용 종료
 int park =0 ;   // 주차 플래그 0: 주차spot에 주차하지 않음, 1: 주차spot에 주차함 
 // LED Matrix 드라이버 제어를 위한 라이브러리
@@ -55,16 +55,26 @@ static const uint8_t PROGMEM bmp_P[]=
 static const uint8_t PROGMEM bmp_D[]=
 { B01111000,B01000100,B01000010,B01000010,B01000010,B01000010,B01000100,B01111000 };//discount
 //
-
 static const uint8_t PROGMEM bmp_badWeather[]=
-{ B00000000,B01100110,B01100110,B00000000,B00011000,B00100100,B01000010,B00000000 };//bad weather
+{ B00000000,B01100110,B01100110,B00000000,B00011000,B00100100,B01000010,B00000000 };
 
-static const uint8_t PROGMEM bmp_money[]=
-{ B00000000,B01000010,B01000010,B01011010,B11111111,B01011010,B01011010,B00100100 };//dollar
+static const uint8_t PROGMEM bmp_goodWeather[]=
+{ B00000000,B01100110,B01100110,B00000000,B10000001,B01000010,B00111100,B00000000 };
 
 static const uint8_t PROGMEM bmp_speedDec[]=
 { B00011000,B00011000,B00011000,B10011001,B01011010,B00111100,B00011000,B00000000 };//아래방향 화살표
 
+static const uint8_t PROGMEM bmp_B_full[]=
+{ B00011000,B01111110,B01111110,B01111110,B01111110,B01111110,B01111110,B01111110 };//battery capacity
+
+static const uint8_t PROGMEM bmp_B_80[]=
+{ B00011000,B01111110,B01000010,B01000010,B01111110,B01111110,B01111110,B01111110 };//battery capacity
+
+static const uint8_t PROGMEM bmp_B_40[]=
+{ B00011000,B01111110,B01000010,B01000010,B01000010,B01000010,B01111110,B01111110 };//battery capacity
+
+static const uint8_t PROGMEM bmp_B_15[]=
+{ B00011000,B01111110,B01000010,B01000010,B01000010,B01000010,B01000010,B01111110 };//battery capacity
 
 
 // 킥보드 상태 초기화 함수
@@ -72,127 +82,124 @@ void pm_init(){
   
   state = 0;  // 킥보드의 동작 상태를 표현하는 변수 0: /on 1:running/ stop: 2/ off: 4 
   cost = 0;   // 요금 변수 
+  battery = 100;
+  battery_print = 0;
   finish = 0;  // 킥보드의 사용이 종료됐음을 표현하는 변수 0: 동작중 1: 사용 종료
   park = 0;   // 주차 플래그 0: 주차spot에 주차하지 않음, 1: 주차spot에 주차함 
   matrix.setRotation(0);
   matrix.clear();
-  delay(1500);
+  delay(500);
   return;
 }
 
 // 킥보드 동작 함수
 void pm_on(){
-  // 헬멧 미착용시: 매트릭스에 S를 띄우고 state = 0->2
-    if((state == 0 || state == 2) && rx_data =="0"){
-                state = 2;
-                matrix.setRotation(0); 
-                matrix.clear();
-                matrix.drawBitmap(0,0,bmp_S,8,8,128);
-                //S 만 출력
-    }
-// 헬멧 착용시 : 매트릭스에 R을 띄우고 시리얼 송신: 1 (킥보드 사용시작 : state =>1)
-    else if ((state == 0 || state == 2) && rx_data == "1"){
-                state = 1;
+// 송신값에 따른 동작부
+  if((state == 0 || state == 2) &&rx_data =="0")
+    state =2;
+
+  else if (battery <10 && norepeat_B <=1 ) {
+    Serial.println(6);
+    norepeat_B++; 
+  }
+  // 사용자가 배터리 정보를 확인하려 할때
+  else if((state==0 || state == 1 || state == 2) && battery_print==1) {
+    matrix.setRotation(0);
+    matrix.clear();
+    if(battery <=100 && battery >= 70)
+            matrix.drawBitmap(0,0,bmp_B_full,8,8,128);
+    else if(battery < 70 && battery >=40)
+            matrix.drawBitmap(0,0,bmp_B_80,8,8,128);
+    else if(battery < 40 && battery >=20)
+            matrix.drawBitmap(0,0,bmp_B_40,8,8,128);
+    else if(battery < 20 && battery >=0 ){
+            matrix.drawBitmap(0,0,bmp_B_15,8,8,128);
+      }
+    delay(2500);
+    battery_print = 0;
+  }
+  else if((state == 0 || state == 2) && rx_data == "1"){
+      state = 1;
                 finish =0;                
                 tx_data = 1;
                 Serial.println(tx_data);
-                
-                matrix.setRotation(0); 
-                matrix.clear();
-                matrix.drawBitmap(0,0,bmp_R,8,8,128); 
-    }
-// 횟수 초과시: state를 4로 바꾸는 동시에 finish도 1로 바꿈, tx_data는 4로 바꿔 사용할 수 없음을 사운드로 알림 
-    else if(state == 2 && rx_data == "-1"){
+  }
+  else if(state == 2 && rx_data == "-1"){
                 tx_data = 4;
                 state = 4;
                 finish = 1;
                 Serial.println(tx_data);
-    }
-// 종료시    : 사용자가 종료버튼을 눌러 rx_data가 4인 경우 
-    else if((state == 1 || state == 3) && rx_data == "4"){
-      // 주차 스팟 외부에서 주차하여, 디스카운트 없이 종료될때
-      if(state ==1){
-        Serial.println(cost);
-        //matrix.setRotation(0);
-        //matrix.clear();
-        //matrix.drawBitmap(0,0,bmp_OFF,8,8,128);
       }
-      // 주차 스팟에서 주차하여 디스카운트와 함께 종료될때   
-      else if(state == 3){
-        Serial.println(state);
-        cost -= 500;
-        Serial.println(cost);
-        matrix.setRotation(0);
-        matrix.clear();
-        matrix.drawBitmap(0,0,bmp_P,8,8,128);
-        //delay(1500);
-        //matrix.clear();
-        //matrix.drawBitmap(0,0,bmp_OFF,8,8,128);
-      }
-        state =4; 
-    }
-    else if(state == 4){
-        matrix.setRotation(0);
-        matrix.clear();
-        matrix.drawBitmap(0,0,bmp_OFF,8,8,128);
-      }
-    else if(state == 0){
-        matrix.setRotation(0);
-        matrix.clear();
-        matrix.drawBitmap(0,0,bmp_ON,8,8,128);
-              
-    }
-    return;
+  else if((state == 1 || state == 3) && rx_data == "4"){
+       // 주차 스팟 외부에서 주차하여, 디스카운트 없이 종료될때
+          if(state ==1){
+          }
+        // 주차 스팟에서 주차하여 디스카운트와 함께 종료될때   
+          else if(state == 3){
+            Serial.println(state);
+            cost -= 500;
+            matrix.setRotation(0);
+            matrix.clear();
+            matrix.drawBitmap(0,0,bmp_P,8,8,128);
+            delay(3000);
+           }          
+  Serial.println(cost); 
+  state =4; 
   }
+  else if(state ==2 && rx_data == "4"){
+  Serial.println(0);     
+  state =4; 
+}    
+  //날씨
+  else if(rx_data == "7"){
+          matrix.setRotation(0);
+          matrix.clear();
+            matrix.drawBitmap(0,0,bmp_goodWeather,8,8,128);
+            delay(3000);
+    
+    }
+// 상태값에 따른 출력부
+  if(state == 0){
+            
+  matrix.setRotation(0);
+  matrix.clear();
+            matrix.drawBitmap(0,0,bmp_ON,8,8,128);
+  }
+  else if(state ==1){
+   
+  matrix.setRotation(0);
+  matrix.clear();
+        matrix.drawBitmap(0,0,bmp_R,8,8,128);
+  }
+  else if(state ==2){
+  
+  matrix.setRotation(0);
+  matrix.clear();
+                  matrix.drawBitmap(0,0,bmp_S,8,8,128);
+  }
+  else if(state == 4){
+  matrix.setRotation(0);
+  matrix.clear();
+            matrix.drawBitmap(0,0,bmp_OFF,8,8,128);
+  }      
+  return;
+  }
+
+
 //BLE server : Data receive part
 class MyCallbacks: public BLECharacteristicCallbacks {   //RX
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string value = pCharacteristic->getValue();
 
       if (value.length() > 0) {
-        Serial.println("*********");
-        Serial.print("New value: ");
-        for (int i = 0; i < value.length(); i++)
-        {            
-          Serial.print(value[i]);
-          if (value[0]=='B' && state<3 ) // 킥보드 배터리 양 출력
-          {
-            if (value[1]=='A')
-            {
-              matrix.setRotation(0);
-              matrix.clear();
-              matrix.drawBitmap(0,0,bmp_B,8,8,128);
-              delay(2000);
-              if (state==0)
-              {
-                matrix.clear();
-                matrix.drawBitmap(0,0,bmp_ON,8,8,128);
-              }
-              else if (state==1)
-              {
-                matrix.clear();
-                matrix.drawBitmap(0,0,bmp_R,8,8,128);
-              }
-              else if (state==2)
-              {
-                matrix.clear();
-                matrix.drawBitmap(0,0,bmp_S,8,8,128);
-              }
-            }
+        if(value == "BATTERY"){
+            battery_print=1;
           }
-          else if (value[0]=='P')  // 주차 SPOT에 가서 주차함 
-          {
-            if (value[1]=='A')
-            {
-              if (state==1)
-              {
-                state=3;
-              }
-            }
-          }
-        }
-        Serial.println();
-        Serial.println("*********");
+         else if(value == "PARK"){
+           if(state ==1){
+              state =3;
+           }
+         } 
       }
     }
 };
@@ -243,7 +250,10 @@ void loop() {
 // 킥보드의 상태가 running일 경우 요금은 계속 100원씩 증가 
     if(state == 1){
       cost += 100;
-      battery -= 1;
+      battery -= 7;
+      if(battery <= 0)
+        battery = 0;
     }
-  delay(2000);
+    delay(2000);
+  //Serial.println(state);
 }
